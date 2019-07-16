89c0a0ce41d0212d211f6742d8bfc69d842e8fd4
wuhao930406 <1148547900@qq.com>
2019-07-16 20:46:14 +0800

    pushed

--- app/public/WxHxd/js/index.js
+++ app/public/WxHxd/js/index.js
@@ -1,14 +1,34 @@
-layui.use(['carousel','layer', 'form','element'], function(){
-    var layer = layui.layer
-    ,form = layui.form;
-    
-    layer.msg('Hello World');
-    var carousel = layui.carousel
-    ,form = layui.form;
-    var element = layui.element;
-    //常规轮播
-    carousel.render({
-      elem: '#test1'
-      ,arrow: 'always'
+layui.use(['carousel', 'layer', 'form', 'element'], function () {
+  var layer = layui.layer
+    , form = layui.form;
+  var carousel = layui.carousel
+    , form = layui.form;
+  var element = layui.element;
+
+  var ins = carousel.render({
+    elem: '#test1'
+    , arrow: 'none'
+  });
+
+  $("#test1").on("touchstart", function (e) {
+    ins.autoplay(false)
+    var startX = e.originalEvent.targetTouches[0].pageX;//????X
+    $(this).on('touchmove', function (e) {
+      arguments[0].preventDefault();//???????????
+    });
+    $(this).on('touchend', function (e) {
+      ins.autoplay(true)
+      var endX = e.originalEvent.changedTouches[0].pageX;//????X
+      e.stopPropagation();//??DOM????????
+      if (endX - startX > 30) {
+        ins.slide("sub");
+      }
+      else if (startX - endX > 30) {
+        ins.slide("add");
+      }
+      $(this).off('touchmove touchend');
     });
   });
+
+
+});
